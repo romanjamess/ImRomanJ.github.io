c71a4cb6f4da22b12cbe401f1f6d8ed1954240e6
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-05-05 20:28:50 -0500

    added links and took out logo

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -25,21 +25,9 @@ export const Projects = () => {
       description: "Design & Development",
       imgUrl: projImg3,
     },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
+   
+   
+   
   ];
 
   return (
