c09a9c06f261bcabebd151b962230638b81adb20
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-05-11 16:33:07 -0500

    fixed navBar issue with Projects link

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -31,7 +31,7 @@ export const Projects = () => {
   ];
 
   return (
-    <section className="project" id="project">
+    <section className="project" id="projects">
       <Container>
         <Row>
           <Col size={12}>
